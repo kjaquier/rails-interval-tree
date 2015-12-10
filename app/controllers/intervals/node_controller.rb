84886ef4e9b446edf6111ad166e9ae8911294352
Hadrien Froger <hadrien.froger@realproject.ch>
2015-12-10 14:08:51 +0100

    add some comments to controllers.

--- app/controllers/intervals/node_controller.rb
+++ app/controllers/intervals/node_controller.rb
@@ -1,5 +1,6 @@
 class Intervals::NodeController < ApplicationController
-
+  ##
+  # Destroy the node and redirect to the tree visualisation
   def destroy
     load_node
     @node.destroy
@@ -7,14 +8,21 @@ class Intervals::NodeController < ApplicationController
                 success: "Node deleted"
   end
 
+
+  ##
+  # Create a new node inside the tree.
   def create
     unless build_node and save_node
-      # TODO add some logic to render error on created node.
+      # TODO add some logic to render error on new tree child we wanted
+      # to create.
     end
   end
 
 private
 
+  ##
+  # Get the parameter for the node.
+  # If the parameters is empty or not exists, return empty array.
   def node_params
     node_params = params[:node]
     node_params ? node_params.permit(:label,
@@ -24,19 +32,29 @@ private
                                      :root_id) : {}
   end
 
+  ##
+  # Give the node's scope the controller can working on.
   def node_scope
     Node::AsNode.all
   end
 
+  ##
+  # Load a node by the id given in parameter.
+  # Raise exception if the id parameter is not given.
   def load_node
     @node ||= node_scope.find(params.require(:id))
   end
 
+  ##
+  # Build a node from param, or give a new node if no parameter
+  # is given.
   def build_node
     @node ||= node_scope.build.as_node
     @node.attributes = node_params
   end
 
+  ##
+  # Save the node and redirect to the tree visualisation.
   def save_node
     @node.save and redirect_to intervals_root_path(@node.root_id)
   end
--- app/controllers/intervals/root_controller.rb
+++ app/controllers/intervals/root_controller.rb
@@ -1,19 +1,27 @@
 class Intervals::RootController < ApplicationController
-
+  ##
+  # Show the tree creation form and the trees already created.
   def index
     build_tree and load_trees
   end
 
+  ##
+  # Show the wanted tree.
   def show
     load_tree
   end
 
+  ##
+  # destroy the tree, and go back to the root page.
   def destroy
     load_tree
     @tree.destroy
     redirect_to root_path, success: "Tree deleted"
   end
 
+  ##
+  # Create a new tree and visualise it.
+  # Display errors if data for creation are not valid.
   def create
     unless build_tree and save_tree
       load_trees and render :index
@@ -22,28 +30,42 @@ class Intervals::RootController < ApplicationController
 
 private
 
+  ##
+  # Node all the trees disponible.
   def load_trees
     @trees ||= tree_scope
   end
 
+  ##
+  # Load the tree wanted by the parameter ´id´.
+  # Rais exception if parameter is omited, or root node not found.
   def load_tree
     @tree ||= tree_scope.find(params.require(:id))
   end
 
+  ##
+  # Build the tere from the parameter given.
+  # If no parameter given, give a new tree.
   def build_tree
     @tree ||= tree_scope.build
     @tree.attributes = tree_params
   end
 
+  ##
+  # Save the tree, and redirect to the visualisation of the tree.
   def save_tree
     @tree.save and redirect_to intervals_root_path(@tree.id)
   end
 
+  ##
+  # Parameter permitted on manipulating a tree.
   def tree_params
     tree_params = params[:node]
     tree_params ? tree_params.permit(:label) : {}
   end
 
+  ##
+  # The scope the controller is working on for tree.
   def tree_scope
     Node::AsRoot.all
   end
